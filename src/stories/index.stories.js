2a60f69a6e15ee992ef3bae6a43fda4c9d8b5c29
CindyvdVries <cindyvdvries@gmail.com>
2021-10-19 15:12:19 +0200

    first setup

---
+++ src/stories/index.stories.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { action } from '@storybook/addon-actions'
+
+import MyButton from '../components/MyButton.vue'
+
+export default {
+  title: 'Button'
+}
+
+export const withText = () => ({
+  components: { MyButton },
+  template: '<my-button @click="action">Hello Button</my-button>',
+  methods: { action: action('clicked') }
+})
+
+export const withSomeEmoji = () => ({
+  components: { MyButton },
+  template: '<my-button>😀 😎 👍 💯</my-button>'
+})
