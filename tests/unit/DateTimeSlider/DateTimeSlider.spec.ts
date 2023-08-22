4ea84a3f5ce8a65f39b9e059e99593b82ec50984
Cees Voesenek <cees.voesenek@vortech.nl>
2023-08-22 16:34:27 +0200

    Add option to make date/time slider floating

--- tests/unit/DateTimeSlider/DateTimeSlider.spec.ts
+++ tests/unit/DateTimeSlider/DateTimeSlider.spec.ts
@@ -1,12 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 import DateTimeSlider from '@/components/DateTimeSlider'
 import { Default } from '@/components/DateTimeSlider/DateTimeSlider.stories'
+import Vuetify from 'vuetify'
 
 describe('DateTimeSlider', () => {
   let vm: any
   let wrapper: any
   beforeEach(() => {
+    const vuetify = new Vuetify()
     wrapper = shallowMount(DateTimeSlider, {
+      vuetify,
       propsData: Default.args
     })
     vm = wrapper.vm
@@ -20,6 +23,6 @@ describe('DateTimeSlider', () => {
   it('emits input', () => {
     vm.$emit = jest.fn()
     vm.toggleNow()
-    expect(vm.$emit.mock.calls[0]).toEqual(['update:now', false])
+    expect(vm.$emit.mock.calls[1]).toEqual(['update:now', false])
   })
 })
