19daa02ffdd6c9f71878d5b7e494b751deb260f5
Werner Kramer <werner.kramer@deltares.nl>
2026-07-31 15:55:13 +0200

    fix: update import path for module federation configuration

--- packages/micro-frontends/vite.config.ts
+++ packages/micro-frontends/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { federation } from '@module-federation/vite'
 import vuetify from 'vite-plugin-vuetify'
-import mfConfig from './module-federation.config.ts'
+import mfConfig from './module-federation.config'
 
 // https://vite.dev/config/
 export default defineConfig({
