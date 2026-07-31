84a06803fb3a12404f8d1a21a97e77d22ceb389b
Werner Kramer <werner.kramer@deltares.nl>
2026-07-31 14:58:19 +0200

    chore: update Storybook setup to include Vuetify and add CSS module declaration

--- packages/components/src/plugins/vuetify.ts
+++ packages/components/src/plugins/vuetify.ts
@@ -1,4 +1,5 @@
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -6,4 +7,7 @@ import * as directives from 'vuetify/directives'
 export default createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
 })
