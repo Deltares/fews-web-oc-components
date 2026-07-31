263b339f9d2f2212a019046671df04033e8fa1d3
Werner Kramer <werner.kramer@deltares.nl>
2026-07-31 13:24:25 +0200

    refactor: migrate Vuetify setup to use createVuetify and streamline imports

--- packages/components/src/plugins/vuetify.ts
+++ packages/components/src/plugins/vuetify.ts
@@ -1,11 +1,9 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib/framework'
-import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-Vue.use(Vuetify)
-
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi'
-  }
+export default createVuetify({
+  components,
+  directives,
 })
