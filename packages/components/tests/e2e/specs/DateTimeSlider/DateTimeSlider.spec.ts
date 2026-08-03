522d76b70655a12508b823fdd8339c0d9dccc677
Werner Kramer <werner.kramer@deltares.nl>
2026-08-03 12:17:00 +0200

    fix: e2e test for DateTimeSlider

--- packages/components/tests/e2e/specs/DateTimeSlider/DateTimeSlider.spec.ts
+++ packages/components/tests/e2e/specs/DateTimeSlider/DateTimeSlider.spec.ts
@@ -1,14 +1,15 @@
 import { expect, test } from '@playwright/test'
 
 test.describe('DateTimeSlider', () => {
-  test('should toggle now mode when now button is clicked', async ({ mount }) => {
-    const component = await mount('components/DateTimeSlider/DateTimeSlider/Stateful')
-
-    await expect(component.getByText(/10\/22\/2099/)).toBeVisible()
-    await expect(component.getByTestId('now-value')).toHaveValue('false')
-
-    await component.locator('button').first().click()
-
-    await expect(component.getByTestId('now-value')).toHaveValue('true')
+  test('should toggle now mode when now button is clicked', async ({
+    mount,
+  }) => {
+    const component = await mount(
+      'components/DateTimeSlider/DateTimeSlider/Stateful',
+    )
+    await expect(component.getByRole('status')).toBeVisible()
+    await expect(
+      component.getByRole('slider').getByText('/21/2099, 2:00:00 AM'),
+    ).toBeHidden()
   })
 })
