21dc190e4e42b0908caa2cdf98f5a2b2da7bc4e0
Werner Kramer <werner.kramer@deltares.nl>
2026-08-03 16:52:39 +0200

    Set correct base for Vitepress and Storybook

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,28 +1,31 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import type { StorybookConfig } from '@storybook/vue3-vite'
+import { mergeConfig } from 'vite'
 
-import { dirname } from "node:path"
+import { dirname } from 'node:path'
 
-import { fileURLToPath } from "node:url"
+import { fileURLToPath } from 'node:url'
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string) {
   return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-vitest'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding')
+    getAbsolutePath('@storybook/addon-onboarding'),
   ],
-  "framework": getAbsolutePath('@storybook/vue3-vite')
-};
-export default config;
+  framework: getAbsolutePath('@storybook/vue3-vite'),
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      base: process.env.STORYBOOK_BASE ?? '/',
+    })
+  },
+}
+export default config
